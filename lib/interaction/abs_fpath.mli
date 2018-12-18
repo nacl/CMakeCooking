9d6b61cc954e49fdb62ab05a57bcdcf28b15bd79
Jesse Haber-Kucharsky <jesse@haberkucharsky.com>
2018-12-18 09:05:45 -0500

    Reformat comment blocks to a width of 120

--- lib/interaction/abs_fpath.mli
+++ lib/interaction/abs_fpath.mli
@@ -1,5 +1,4 @@
-(** Just like {! Fpath}, but with run-time checks that the path indicated is
-   absolute (not relative). *)
+(** Just like {! Fpath}, but with run-time checks that the path indicated is absolute (not relative). *)
 
 (** An absolute file-system path. *)
 type t
@@ -9,21 +8,19 @@ val pp :
 
 val check :
   Fpath.t -> (t, [> `Msg of string]) result
-(** Construct an absolute path. The result is an error if the indicated path is
-   not absolute. *)
+(** Construct an absolute path. The result is an error if the indicated path is not absolute. *)
 
 val extract :
   t -> Fpath.t
-(** Extract the underlying {! Fpath.t} with the knowledge that it is an absolute
-   path. *)
+(** Extract the underlying {! Fpath.t} with the knowledge that it is an absolute path. *)
 
 (* Copyright 2018 Jesse Haber-Kucharsky
 
- Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
-   License. You may obtain a copy of the License at
+   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+   the License. You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+   http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
-   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+   an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
    specific language governing permissions and limitations under the License. *)
--- lib/model/directed_graph.mli
+++ lib/model/directed_graph.mli
@@ -1,5 +1,4 @@
-(** Construct a module for a directed graph with specific types for node labels
-    and associated values. *)
+(** Construct a module for a directed graph with specific types for node labels and associated values. *)
 module Make :
   functor
     (K : Directed_graph_intf.LABEL)
@@ -8,11 +7,11 @@ module Make :
 
 (* Copyright 2018 Jesse Haber-Kucharsky
 
- Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
-   License. You may obtain a copy of the License at
+   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+   the License. You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+   http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
-   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+   an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
    specific language governing permissions and limitations under the License. *)
